67603d12e13078d7b057f8cb23455bc9716f2cc6
yuroich <nathanielraynada1@gmail.com>
2026-05-14 21:45:31 +0800

    Updated admin operators and interface

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bicoltransit-v1';
+const CACHE_NAME = 'bicoltransit-v3';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -11,9 +11,21 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
